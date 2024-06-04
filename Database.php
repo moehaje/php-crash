6663f40f3c537b82cd708b272fead5b3ca158fa0
Moe Haje <91034104+moehaje@users.noreply.github.com>
2024-06-04 13:03:04 +0200

    php-learnings

---
+++ Database.php
@@ -0,0 +1,22 @@
+<?php
+
+// connect to MySQL database, and execute a query
+class Database {
+    public $connection;
+    public function __construct($config, $username = 'root', $password = '') {
+        // $dsn = 'mysql:host=' . $config['host'] . ';port=' . $config['port'] . ';dbname=' . $config['database'] . ';charset=' . $config['charset'];
+        $dsn = 'mysql:' . http_build_query($config, '', ';');  // host=localhost;port=3306;dbname=myapp
+        $this->connection = new PDO($dsn, $username, $password, [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+    }
+    public function query($query, $params = []) {
+
+        $statement = $this->connection->prepare($query);
+        $statement->execute($params);
+
+        return $statement;
+    }
+}
+
+
