62f41bdd17492269c3b8255aca2245c666901ba5
Moe Haje <91034104+moehaje@users.noreply.github.com>
2024-06-05 21:44:54 +0200

    Extend helper functions: Add abort, authorized, base_path, and view functions; integrate Response class

---
+++ Core/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Core\Response;
+
+function dd($value){
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+
+    die();
+}
+
+function abort($code = 404) {
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();
+}
+
+function isUrl($value){
+    return $_SERVER["REQUEST_URI"] === $value;
+}
+
+function authorized($condition, $status = Response::FORBIDDEN){
+    if (!$condition){
+        abort($status);
+    }
+}
+
+function base_path($path){
+    return BASE_PATH . $path;
+}
+function view($path, $attributes = []){
+    extract($attributes);
+
+    require base_path('views/' . $path);
+}
