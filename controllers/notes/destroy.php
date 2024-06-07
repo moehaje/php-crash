aeabcd553c73912a915b967d030352e4cabfb7ec
Moe Haje <91034104+moehaje@users.noreply.github.com>
2024-06-07 10:51:34 +0200

    Database Container

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -1,9 +1,13 @@
 <?php
 
-use Core\Database;
+use Core\App;
+// use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+//$config = require base_path('config.php');
+//$db = new Database($config['database']);
+
+// $db = App::container()->resolve('Core\Database');
+$db = App::resolve('Core\Database');
 
 $currentUserId = 1;
 
--- controllers/notes/index.php
+++ controllers/notes/index.php
@@ -1,10 +1,12 @@
 <?php
 
-use Core\Database;
+use Core\App;
+// use Core\Database;
 
-$config = require base_path('config.php');
-$db = new Database($config['database']);
+//$config = require base_path('config.php');
+//$db = new Database($config['database']);
 
+$db = App::resolve('Core\Database');
 $notes = $db->query('select * from notes where user_id = 1')->get();
 
 
